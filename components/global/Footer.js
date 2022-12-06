5da9ba6dd4a22196e4084fc797298054c0416e5d
John <Johnpioc15@gmail.com>
2022-12-06 20:57:30 +1000

    site changes

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -1,25 +1,12 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-
-const socialLinks = [
-    {
-        icon: <FaGithub  className="h-full w-full"/>,
-        link: "https://github.com/Johnnyftb"
-    },
-    {
-        icon: <FaLinkedin className="h-full w-full"/>,
-        link: "https://www.linkedin.com/in/john-pioc/"
-    }
-];
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="h-[20vh] relative bottom-0">
         <div className="px-16 pb-8 flex items-center h-full w-full">
-            {socialLinks.map(item => (
-                <a href={item.link} target="_blank" className="h-10 mr-4">
-                    {item.icon}
-                </a>
-            ))}
+            <a href="https://github.com/Johnnyftb" target="_blank" className="h-10 mr-4">
+                <FaGithub  className="h-full w-full"/>
+            </a>
         </div>
     </footer>
   )
